30b1e6bc7e67044236ef7fbf1a75f77cb8bbcdc4
sbaldassano <stevenbaldassano@gmail.com>
2017-03-27 13:09:33 -0400

    Update clipForAlgoPipeline.m

--- clipForAlgoPipeline.m
+++ clipForAlgoPipeline.m
@@ -1,17 +1,20 @@
 % use this to assemble and save the clips to train classifier
+% it takes in .mat files for each seizure and interictal segment
+
 function clipForAlgoPipeline(fs, ptName)
 
 leads = 1:4;  
-    
+% make a directory for this subject's data    
 if ~exit(ptName,'dir')
     mkdir(ptName)
 end
-
+% set up channels names in correct format
 for i = 1:numel(leads)
     field = ['X_Ch' num2str(i) '_'];
     channels.(field) = ['Ch' num2str(i)];
 end
-    
+
+% load and clip seizures
 szClipNum = 0;
 i=1;
 while 1
@@ -30,7 +33,7 @@ while 1
     i = i+1;      
 end
     
-    
+% load and clip interictals    
 interictalClipNum = 0;
 i=1;
 while 1
@@ -51,6 +54,8 @@ while 1
 end
 end
 
+% this function does the actual clipping. It gets rid of one-second clips that have missing data or dead leads. Saves the one-second clips
+% in the format expected by the algorithm for training
 function netClips = clipData(curData,fs,channels,ptName,numClips,tempClipNum,ictal)
     % will clip the data into one second chunks and then save each of them
     % in the appropriate format
